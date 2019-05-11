1de56053afffbffee015a205b8264a1d747b3578
Feng <joseph.feng1@gmail.com>
2019-05-11 16:07:27 -0600

    random.randint()

--- pyTest.py
+++ pyTest.py
@@ -4,7 +4,7 @@ import random
 def printRandoms():
 		Sum = 0
 		for i in range(0,100):
-			newNum = randint(0,100)
+			newNum = random.randint(0,100)
 			print("Iteration "+ str(i+1)+" >>> "+ str(newNum))
 			Sum += newNum
 		print("Total sum is "+ str(Sum))
